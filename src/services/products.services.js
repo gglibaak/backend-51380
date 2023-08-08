3b14dc098c68767d8d86efd0397f1f35289ebf00
Gabriel GL <gglibaak@gmail.com>
2023-08-08 11:36:11 -0300

    fix

--- src/services/products.services.js
+++ src/services/products.services.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const { ProductsDAO } = require('../model/daos/app.daos');
-const CustomError = require('../utils/errors/custom-error');
+const CustomError = require('../utils/errors/customError');
 const customErrorMsg = require('../utils/errors/customErrorMsg');
 const EErros = require('../utils/errors/enums');
 
@@ -77,28 +77,25 @@ class MongoProducts {
         message: '🛑 Wrong Data Format.',
         code: EErros.INVALID_TYPES_ERROR,
         cause: customErrorMsg.generateProductErrorInfo(data),
-        // isJson: true,
       });
     }
 
-    //check if the product exists by code
     if (await productsDAO.getById(code, true)) {
       return CustomError.createError({
         name: 'Validation Error',
         message: '🛑 The product alredy exists.',
         code: EErros.PRODUCT_ALREADY_EXISTS,
         cause: customErrorMsg.generateProductoErrorAlredyExists(data),
-        // isJson: true,
       });
     }
 
-    const producto = {
+    const product = {
       ...data,
       status: true,
     };
 
-    await productsDAO.add({ ...producto });
-    return { status: 200, result: { status: 'success', payload: producto } };
+    await productsDAO.add({ ...product });
+    return { status: 200, result: { status: 'success', payload: product } };
   }
 
   async updateProduct(id, newDataProduct) {

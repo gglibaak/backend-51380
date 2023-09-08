d89a160a369174c64c4c9c2e1131329df99ac43c
Gabriel GL <gglibaak@gmail.com>
2023-09-08 01:33:59 -0300

    test
    test

--- src/routers/realtimeprods.routes.js
+++ src/routers/realtimeprods.routes.js
@@ -3,6 +3,6 @@ const { isLogged, hasPrivileges } = require('../middlewares/auth');
 const realTimeProdController = require('../controllers/realtimeprods.controller');
 const realTimeProdRoutes = express.Router();
 
-realTimeProdRoutes.get('/', isLogged, hasPrivileges, realTimeProdController.getRealTimeProd);
+realTimeProdRoutes.get('/', /* isLogged, hasPrivileges,*/ realTimeProdController.getRealTimeProd);
 
 module.exports = realTimeProdRoutes;
